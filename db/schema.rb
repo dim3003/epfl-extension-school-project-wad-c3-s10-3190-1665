071c2cda91c42979fc109edf338eea22c2dbe802
Jey3003 <andre.d3003@gmail.com>
2020-07-17 20:41:08 +0200

    add email attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717180526) do
+ActiveRecord::Schema.define(version: 20200717184022) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end

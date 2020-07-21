988ac1a736ca6de4f6fb0cb1296d7f38891267c7
Jey3003 <andre.d3003@gmail.com>
2020-07-21 13:52:06 +0200

    recreate tables in sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200721115111) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

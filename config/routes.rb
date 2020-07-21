af2bac4037ae5f16909ef2a65e59d998f3eff84a
Jey3003 <andre.d3003@gmail.com>
2020-07-21 13:06:24 +0200

    rename the app because model name clash

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-  get 'pins/edit'
-
-  get 'pins/index'
-
-  get 'pins/new'
-
-  get 'pins/show'
-
-  resources :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

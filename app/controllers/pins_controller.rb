9ca1e01b511b36baadb4a385bf293f428831a7a1
Jey3003 <andre.d3003@gmail.com>
2020-07-21 15:59:18 +0200

    controller create and new def

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,8 +7,21 @@ class PinsController < ApplicationController
   end
 
   def new
+    @pin = Pin.new
   end
 
   def show
   end
+
+  def create
+    @pin = Pin.new(pin_params)
+    redirect_to pins_path
+  end
+
+  private
+
+    def pin_params
+      params.require(:pin).permit(:title, :image_url, :tag)
+    end
+    
 end

dfca67a749bdaefa49237ea373e2f5f2da6ac0f2
Jey3003 <andre.d3003@gmail.com>
2020-07-21 15:27:55 +0200

    user pin association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200721115111) do
+ActiveRecord::Schema.define(version: 20200721132554) do
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20200721115111) do
     t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pins_users", id: false, force: :cascade do |t|
+    t.integer "pin_id", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|

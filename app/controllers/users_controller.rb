b2f615c4c6d5c1d15cc7c254e1869a8e64e88aa1
Jey3003 <andre.d3003@gmail.com>
2020-07-22 09:19:30 +0200

    create action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.find_or_create_by(user_params)
+    session[:user_id] = user.id
+    redirect_to pins_path
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
 end

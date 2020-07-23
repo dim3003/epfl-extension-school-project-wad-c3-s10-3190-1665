c442c79ac17777e304c4b22b0c2508ca771b941e
Jey3003 <andre.d3003@gmail.com>
2020-07-23 10:50:43 +0200

    user comment association

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200723084416) do
+ActiveRecord::Schema.define(version: 20200723085014) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pin_id"
     t.string "body"
+    t.integer "user_id"
     t.index ["pin_id"], name: "index_comments_on_pin_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "pins", force: :cascade do |t|

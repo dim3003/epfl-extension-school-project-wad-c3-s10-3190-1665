46400c3f9a3d7535646e4816c0a35e75a66d90ee
Jey3003 <andre.d3003@gmail.com>
2020-07-24 09:50:03 +0200

    edit pin test

--- test/system/pins_test.rb
+++ test/system/pins_test.rb
@@ -2,6 +2,25 @@ require "application_system_test_case"
 
 class PinsTest < ApplicationSystemTestCase
 
+  test 'check homepage pins' do
+    6.times do |i|
+      pin = Pin.new title: "Exciting new pin #{i+1}",
+                      user: User.new
+      pin.save!
+    end
+
+    visit('/')
+
+    assert page.has_content?('Exciting new pin 1')
+    assert page.has_content?('Exciting new pin 2')
+    assert page.has_content?('Exciting new pin 3')
+    assert page.has_content?('Exciting new pin 4')
+    assert page.has_content?('Exciting new pin 5')
+    assert page.has_content?('Exciting new pin 6')
+
+    refute page.has_content?('Exciting new pin 7')
+  end
+
   test 'create new pin' do
     user = User.new email: 'an@email.com'
     user.save!
@@ -12,22 +31,37 @@ class PinsTest < ApplicationSystemTestCase
     fill_in('Title', with: 'Test pin')
     fill_in('Image url', with: 'https://www.gettyimages.co.uk/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg')
     fill_in('Tag', with: 'test tag')
-    click_on('Create pin', match: :first)
+    click_on('Create Pin', match: :first)
     assert page.has_content?('Test pin')
   end
 
   test 'index loads pins' do
     pin_1 = Pin.new title: 'Join a tennis club',
-                      user: User.new
+                    user: User.new
     pin_1.save!
 
     pin_2 = Pin.new title: 'Start a blog',
-                      user: User.new
+                    user: User.new
     pin_2.save!
 
     visit(pins_path)
     assert page.has_content?('Join a tennis club')
     assert page.has_content?('Start a blog')
+  end
+
+  test 'edit pin test' do
+    user = User.new email: 'an@email.com'
+    user.save!
+    pin = Pin.new title: 'edit pin test',
+                  user: User.new
+    pin.save!
+    visit(edit_pin_path(pin))
+    fill_in('Title', with: 'Climb Bishorn')
+    fill_in('Tag', with: 'mountain')
+    click_on('Update', match: :first)
+    click_on('Climb Bishorn', match: :first)
+    assert page.has_content?('Climb Bishorn')
+    assert page.has_content?('mountain')
   end
 
   test 'search' do
@@ -52,20 +86,7 @@ class PinsTest < ApplicationSystemTestCase
     assert page.has_content?('No pin found')
   end
 
-  test 'check homepage pins' do
-    3.times do |i|
-      pin = Pin.new title: "Exciting new pin #{i+1}",
-                      user: User.new
-      pin.save!
-    end
 
-    visit('/')
 
-    assert page.has_content?('Exciting new pin 1')
-    assert page.has_content?('Exciting new pin 2')
-    assert page.has_content?('Exciting new pin 3')
-
-    refute page.has_content?('Exciting new pin 4')
-  end
 
 end

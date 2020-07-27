6c6d97e93f67c328ad58a27cef368a88f9447bf6
Jey3003 <andre.d3003@gmail.com>
2020-07-27 11:33:27 +0200

    navigation tests

--- test/system/navigation_tests_test.rb
+++ test/system/navigation_tests_test.rb
@@ -2,20 +2,46 @@ require "application_system_test_case"
 
 class NavigationTestsTest < ApplicationSystemTestCase
 
-  test 'navigation test' do
+  test 'navigation test not logged in' do
     visit('/')
-    expect(page).to have_selector(:link_or_button, 'Pin')
-    expect(page).to have_selector(:link_or_button, 'All Pins')
-    expect(page).to have_selector(:link_or_button, 'Sign Up / Log in')
-    expect(page).to have_selector(:link_or_button, 'About')
+    assert page.has_selector?(:link_or_button, 'Pin')
+    assert page.has_selector?(:link_or_button, 'All Pins')
+    assert page.has_selector?(:link_or_button, 'Sign up / Login')
+    assert page.has_selector?(:link_or_button, 'About')
 
-    click_on('All Pins', match: :first)
-    expect(page).to have_selector(:link_or_button, 'Pin')
-    expect(page).to have_selector(:link_or_button, 'All Pins')
-    expect(page).to have_selector(:link_or_button, 'Sign Up / Log in')
-    expect(page).to have_selector(:link_or_button, 'About')
-  end
+    visit(pins_path)
+    assert page.has_selector?(:link_or_button, 'Pin')
+    assert page.has_selector?(:link_or_button, 'All Pins')
+    assert page.has_selector?(:link_or_button, 'Sign up / Login')
+    assert page.has_selector?(:link_or_button, 'About')
 
+    visit(new_user_path)
+    assert page.has_selector?(:link_or_button, 'Pin')
+    assert page.has_selector?(:link_or_button, 'All Pins')
+    assert page.has_selector?(:link_or_button, 'Sign up / Login')
+    assert page.has_selector?(:link_or_button, 'About')
+    end
 
+    test 'navigation test logged in' do
+
+      user = User.new email: 'an@email.com'
+      user.save!
+      visit(new_user_path)
+      fill_in('Email address', with: user.email)
+      click_on('Log in', match: :first)
+
+      visit('/')
+      assert page.has_selector?(:link_or_button, 'Pin')
+      assert page.has_selector?(:link_or_button, 'All Pins')
+      assert page.has_selector?(:link_or_button, 'My Pins')
+      assert page.has_selector?(:link_or_button, 'About')
+
+      visit(pins_path)
+      assert page.has_selector?(:link_or_button, 'Pin')
+      assert page.has_selector?(:link_or_button, 'All Pins')
+      assert page.has_selector?(:link_or_button, 'My Pins')
+      assert page.has_selector?(:link_or_button, 'About')
+
+      end
 
 end
